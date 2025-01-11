b93d951c5d9eb442ed45413d22f4360489b887ce
fuwariko <cessedimeow@gmail.com>
2025-01-11 15:35:12 +0500

    fix

---
+++ src/consts/filter.ts
@@ -0,0 +1,4 @@
+export const FILTER_TITLE_POPULAR = 'Popular';
+export const FILTER_TITLE_PRICE_LOW_TO_HIGH = 'Price: low to high';
+export const FILTER_TITLE_PRICE_HIGH_TO_LOW = 'Price: high to low';
+export const FILTER_TITLE_TOP_RATED_FIRST = 'Top rated first';
